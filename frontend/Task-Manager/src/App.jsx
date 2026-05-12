91e57730557d6be686c20bcbe497217df6f01715
ShivanshuPandey <Shivanshupandey9291@gmail.com>
2026-05-13 01:22:54 +0530

    Enhance task management features by adding task creation logic, improving error handling, and implementing attachment input functionality. Update styles for buttons and layout adjustments in ManageTasks and CreateTask components.

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -11,38 +11,48 @@ import CreateTask from "./pages/Admin/CreateTask";
 import UserDashboard from "./pages/User/UserDashboard";
 import PrivateRoute from "./routes/PrivateRoute";
 import UserProvider, { UserContext } from "./context/userContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return(
     <UserProvider>
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
 
-          {/* Admin Routes */}
-          <Route element={<PrivateRoute allowedRoles={["admin"] } />}>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/tasks" element={<ManageTasks />} />
-            <Route path="/admin/create-task" element={<CreateTask />} />
-            <Route path="/admin/users" element={<ManageUsers />} />
-          </Route>
-          
-          {/* User Routes */}
-          <Route element={<PrivateRoute allowedRoles={["user"] } />}>
-            <Route path="/user/dashboard" element={<UserDashboard />} />
-            <Route path="/user/tasks" element={<MyTasks />} />
-            <Route 
-            path="/user/task-details/:id" 
-            element={<ViewTaskDetails />} 
-            />
-          </Route>
-          {/* Default Route */}
-          <Route path="/" element={<Root/>}/>
-        </Routes>
-      </Router>
-    </div>
+            {/* Admin Routes */}
+            <Route element={<PrivateRoute allowedRoles={["admin"] } />}>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/tasks" element={<ManageTasks />} />
+              <Route path="/admin/create-task" element={<CreateTask />} />
+              <Route path="/admin/users" element={<ManageUsers />} />
+            </Route>
+            
+            {/* User Routes */}
+            <Route element={<PrivateRoute allowedRoles={["user"] } />}>
+              <Route path="/user/dashboard" element={<UserDashboard />} />
+              <Route path="/user/tasks" element={<MyTasks />} />
+              <Route 
+              path="/user/task-details/:id" 
+              element={<ViewTaskDetails />} 
+              />
+            </Route>
+            {/* Default Route */}
+            <Route path="/" element={<Root/>}/>
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOptions={{
+          className:"",
+          style:{
+            fontSize:"13px",
+          },
+        }}
+      />
     </UserProvider>
   );
 };
